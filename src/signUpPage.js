874aed41d5b0a67035e33f44cf2a21548cc1e73b
Soham Das <soham5959595@gmail.com>
2018-02-11 20:12:17 +0530

    Added multiple states

--- src/signUpPage.js
+++ src/signUpPage.js
@@ -8,20 +8,29 @@ class SignUpPage extends React.Component {
         super(props);
         this.state = {
             username: '',
-            password: ''
+            email: '',
+            password: '',
+            passwordConfirmation: ''
         }
 
         this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
     }
 
     onChange(e) {
         this.setState({[e.target.name]: e.target.value});
     }
 
+    onSubmit(e) {
+        e.preventDefault();
+        console.log(this.state);
+    }
+
     render() {
         return (
             <div className="container">
-                <form className="form-space" method="post">
+                <form className="form-space" onSubmit={this.onSubmit}>
                     <h1>Login to your account</h1>
                     <div className="row">
                         <div className="col-md-4"></div>
@@ -38,9 +47,32 @@ class SignUpPage extends React.Component {
                         <div className="col-md-4"></div>
                         <div className="col-md-4">
                             <div className="form-group">
+                                <label className="form-control-lg">Email:</label>
+                                <input value={this.state.email} onChange={this.onChange} type="text"
+                                       className="form-control" placeholder="Email" name="email"
+                                       required/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-4"></div>
+                        <div className="col-md-4">
+                            <div className="form-group">
                                 <label className="form-control-lg">Password:</label>
-                                <input value={this.state.password} onChange={this.onChange} type="text"
+                                <input value={this.state.password} onChange={this.onChange} type="password"
                                        className="form-control" placeholder="Password" name="password"
+                                       required/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-4"></div>
+                        <div className="col-md-4">
+                            <div className="form-group">
+                                <label className="form-control-lg">Confirm Password:</label>
+                                <input value={this.state.passwordConfirmation} onChange={this.onChange} type="password"
+                                       className="form-control" placeholder="Confirm Password"
+                                       name="passwordConfirmation"
                                        required/>
                             </div>
                         </div>
