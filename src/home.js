b33f1009a2de7ba088006464e7a04bf068a24e76
Soham Das <soham5959595@gmail.com>
2018-02-25 15:07:03 +0530

    Added Home template. Added sticky-footer.

---
+++ src/home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="jumbotron">
+                    <div className="container text-center">
+                        <h1>Online Store</h1>
+                        <p>Mission, Vission & Values</p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
