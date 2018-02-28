b108bc22ba3a3ca6279c8b492737f407fcf36f0a
Soham Das <soham5959595@gmail.com>
2018-02-28 23:13:56 +0530

    Get requests working. Need to introduce delete and update requests.

--- src/navigationBar.js
+++ src/navigationBar.js
@@ -6,7 +6,7 @@ class NavigationBar extends Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-sm navbar-dark bg-custom">
+                <nav className="navbar navbar-expand-sm navbar-dark bg-custom fixed-top">
                     <div className="container-fluid">
                         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-content" aria-controls="nav-content" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
