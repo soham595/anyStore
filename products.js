a5c23328378da6d3a828c7b26e9566bdc668fdc4
Soham Das <soham5959595@gmail.com>
2018-03-04 15:52:32 +0530

    Products, unwanted field response removed, count added. Still improving response messages.

--- products.js
+++ products.js
@@ -25,11 +25,15 @@ router.post('/', (req, res) => {
 
 router.get('/', (req, res) => {
     Product.find()
+        .select('name price _id')
         .exec()
         .then(docs => {
-            console.log(docs);
+            const response = {
+                count: docs.length,
+                products: docs
+            };
             // if (docs.length>=0) {
-            res.status(200).json(docs);
+            res.status(200).json(response);
             //  } else {
             //      res.status(404).json({
             //         message: 'No entries found'
