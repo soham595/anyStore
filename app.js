7f332c9e4185588421a17e4f9ad7052a25c8b37f
Soham Das <soham5959595@gmail.com>
2018-03-08 13:49:23 +0530

    Multer proper functions to store proper image sizes and types. Image routes working. (doubt)

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'));
 
 app.use('/users', users);
 app.use('/products', products);
